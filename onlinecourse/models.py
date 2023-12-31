d8e26164e2ceb8fbcb4a4304e0f511eecc8c3c50
keerat21 <tanwarkeeratcs@gmail.com>
2023-12-31 04:42:55 -0500

    models.py modified

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -9,6 +9,27 @@ except Exception:
 from django.conf import settings
 import uuid
 
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    content = models.CharField(max_lengtth=200)
+    grade=models.IntegerField(default=50)
+
+        # method to calculate if the learner gets the score of the question
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return "Question: "+ self.content
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
 
 # Instructor model
 class Instructor(models.Model):
